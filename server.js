f7aa5235946b39672d3cbd7ec1f124846c298b3f
johnnypires <johnny_po@hotmail.com>
2021-11-15 14:19:25 -0400

    tornar melhor 3

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+const { application } = require('express');
 const express = require('express');
 const nunjucks = require('nunjucks');
 const routes = require('./routes');
 
 const server = express();
 
+const porta = process.env.PORT || 8080;
 
 server.use(express.static('public'));
 server.use(routes);
@@ -20,8 +22,10 @@ server.get("/", function(req, res) {
     return res.render("index")
 })
 
+server.listen(porta);
+/*
 server.listen(3000, function() {
     console.log("server is running")
-})
+})*/
 
 
